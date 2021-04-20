181cfe61c7be0052d2bae70343cd23cab16a5fa2
dani-d-dev <dani.daverio.dev@gmail.com>
2021-04-20 10:07:04 +0200

    Update api key  and set as an environment variable

--- Movies/Constants/Constants.swift
+++ Movies/Constants/Constants.swift
@@ -13,7 +13,7 @@ struct Constants {
     struct API {
         static let baseURL = URL(string: "https://api.themoviedb.org")!
         static let baseImageURL = URL(string: "https://image.tmdb.org/t/p/w500")!
-        static let apiKey = "8b2cea5b14d8ec7d3d4c76b5480d012c"
+        static let apiKey: String = ProcessInfo.processInfo.environment["api_key"] ?? ""
         static let apiKeyName = "api_key"
         static let discover = "/3/discover/movie"
         static let search = "/3/search/movie"
